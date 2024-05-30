ee314f0e56a17e6d9f5ef3c1d1243c046d351c71
bl0v3 <bl0v3@bl0v3.mailme.at.bl0v3_followed_by_dot_and_com>
2024-05-30 15:24:16 +0200

    "declarative-build"

---
+++ flake.nix
@@ -0,0 +1,31 @@
+{
+  inputs = {
+    bl0v3_static = {
+      url = "github:bolives-hax/bl0v3.com-static";
+    };
+    flake-utils.url = "github:numtide/flake-utils";
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+  };
+
+  outputs = {self,bl0v3_static,flake-utils,nixpkgs}: flake-utils.lib.eachDefaultSystem(system: 
+  let
+    pkgs = import nixpkgs {inherit system;};
+  in {
+    packages.default = pkgs.writeShellScriptBin "update-site" ''
+      SITE_PATH="${bl0v3_static.packages.${system}.github}"
+      echo $SITE_PATH
+      if [ -z "$1" ]; then
+        echo usage: nix run .# \"git commit message\"
+      else 
+      cp -Lr --no-preserve=mode $SITE_PATH site
+      cp -r .git flake.nix site/
+      cd site
+      git add .
+      git commit -m \"$1\"
+      git push
+      cd ../
+      rm -rf site
+      fi
+    '';
+  });
+}
